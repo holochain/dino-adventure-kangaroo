add6ee38362972db261cbe53cb91c02bc60d48ae
ThetaSinner <ThetaSinner@users.noreply.github.com>
2026-01-23 13:16:55 +0000

    ci: disable windows code signing

--- kangaroo.config.ts
+++ kangaroo.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   productName: 'Dino Adventure',
   version: '0.2.1-rc.0',
   macOSCodeSigning: true,
-  windowsEVCodeSigning: true,
+  windowsEVCodeSigning: false,
   fallbackToIndexHtml: true,
   autoUpdates: true,
   systray: true,
