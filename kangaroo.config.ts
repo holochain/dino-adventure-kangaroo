01af2577dc7cae04b5d7dd2db574ee89d9663401
ThetaSinner <ThetaSinner@users.noreply.github.com>
2026-07-22 13:44:41 +0100

    chore: bump version

--- kangaroo.config.ts
+++ kangaroo.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from './src/main/defineConfig';
 export default defineConfig({
   appId: 'org.holochain.dino-adventure',
   productName: 'Dino Adventure',
-  version: '0.3.0-rc.0',
+  version: '0.3.0-rc.1',
   macOSCodeSigning: true,
   windowsEVCodeSigning: false,
   fallbackToIndexHtml: true,
